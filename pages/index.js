af3fe51e163e8301b4c34238eb9940094dfb37d2
ahmed el <ahmedelmeskyny@gmail.com>
2021-07-17 23:02:12 +0100

    home header

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,12 @@ import styles from '../styles/Home.module.scss';
 //components
 import Layout from '../components/layout/layout';
 
+//react-icons
+import {FaFacebookSquare , FaLinkedinIn } from "react-icons/fa";
+import {ImTwitter} from "react-icons/im";
+import {AiFillInstagram} from "react-icons/ai";
+
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -21,11 +27,21 @@ export default function Home() {
           <div className={styles.hero}>
             <div className={styles.heroText}>
               <span className={styles.name}>SNIPER STUDIO</span>
-              <p>It is a long established fact that a reader will be distracted by</p>
+              <div className={styles.slogan} >
+                <Image alt="quote" src="/quote.png" width="30px" height="30px"></Image>
+                <p>It is a long established fact that a reader will be distracted by  that a reader will be  will be  will be  will be</p>
+                <div  className={styles.img}>
+                <Image alt="quote" src="/quote.png" width="30px" height="30px"></Image>
+                </div>
+              </div>
+              <div className={styles.social}>
+                 <FaFacebookSquare className={styles.icon}></FaFacebookSquare>
+                 <FaLinkedinIn className={styles.icon}></FaLinkedinIn>
+                 <ImTwitter className={styles.icon} ></ImTwitter>
+                 <AiFillInstagram className={styles.icon}></AiFillInstagram>
+              </div>
             </div>
-            <div className={styles.heroImage}>
-              <Image alt="sniper studio" src="/bg.png" width="200px" height="300px"></Image>
-            </div>
+            <div className={styles.heroImage}></div>
           </div>
         </div>
       </Layout>
